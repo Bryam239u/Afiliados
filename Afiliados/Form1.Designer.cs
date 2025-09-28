efa619a2ca2c353fb72df7b6f38ffceb2d676b36
Bryam239u <jaramillobryam870@gmail.com>
2025-09-28 15:16:46 -0600

    Agregar archivos de proyecto.

---
+++ Afiliados/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Afiliados
+{
+    partial class FRMAfiliados
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FRMAfiliados));
+            this.SuspendLayout();
+            // 
+            // FRMAfiliados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FloralWhite;
+            this.ClientSize = new System.Drawing.Size(828, 510);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FRMAfiliados";
+            this.Text = "Afiliados";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
